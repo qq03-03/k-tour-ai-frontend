34a7f5b55807a6ce4f703cfa82fc488e66e0ec69
qq03-03 <yeonseopark71@gmail.com>
2026-08-25 14:36:32 +0900

    Revert "fix: configure vitest to support pure function tests with node environment"
    This reverts commit 1a7e64d9e168f57355a4000f9e9dc2d0fd757cfa.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,4 @@
 import '@testing-library/jest-dom'
 import { beforeEach } from 'vitest'
 
-if (typeof window !== 'undefined') {
-  beforeEach(() => window.localStorage.clear())
-}
+beforeEach(() => window.localStorage.clear())
